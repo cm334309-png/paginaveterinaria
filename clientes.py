09cf8db58068296c56efbbf3bd7401579e154950
cm334309-png <molina12camila@hotmail.com>
2026-07-01 14:49:57 -0500

    Actualizar clientes.py para Render

--- clientes.py
+++ clientes.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import sqlite3
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -12,32 +13,22 @@ def crear_bd():
     cursor = conexion.cursor()
 
     cursor.execute("""
-
         CREATE TABLE IF NOT EXISTS clientes(
-
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-
             nombre TEXT,
-
             correo TEXT,
-
             telefono TEXT,
-
             mascota TEXT,
-
             especie TEXT,
-
             mensaje TEXT
-
         )
-
     """)
 
     conexion.commit()
-
     conexion.close()
 
 crear_bd()
+
 
 @app.route("/clientes", methods=["POST"])
 def registrar():
@@ -49,28 +40,23 @@ def registrar():
     cursor = conexion.cursor()
 
     cursor.execute("""
-
         INSERT INTO clientes
         (nombre,correo,telefono,mascota,especie,mensaje)
-
         VALUES(?,?,?,?,?,?)
-
-    """,(
-
+    """, (
         datos["nombre"],
         datos["correo"],
         datos["telefono"],
         datos["mascota"],
         datos["especie"],
         datos["mensaje"]
-
     ))
 
     conexion.commit()
-
     conexion.close()
 
-    return jsonify({"mensaje":"Cita registrada correctamente."})
+    return jsonify({"mensaje": "Cita registrada correctamente."})
+
 
 @app.route("/clientes", methods=["GET"])
 def listar():
@@ -85,24 +71,28 @@ def listar():
 
     conexion.close()
 
-    lista=[]
+    lista = []
 
     for fila in datos:
 
         lista.append({
-
-            "id":fila[0],
-            "nombre":fila[1],
-            "correo":fila[2],
-            "telefono":fila[3],
-            "mascota":fila[4],
-            "especie":fila[5],
-            "mensaje":fila[6]
-
+            "id": fila[0],
+            "nombre": fila[1],
+            "correo": fila[2],
+            "telefono": fila[3],
+            "mascota": fila[4],
+            "especie": fila[5],
+            "mensaje": fila[6]
         })
 
     return jsonify(lista)
 
-if __name__=="__main__":
 
-    app.run(debug=True,port=5003)
+@app.route("/")
+def inicio():
+    return "Microservicio de Clientes funcionando correctamente."
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5003))
+    app.run(host="0.0.0.0", port=port)
